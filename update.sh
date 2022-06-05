294569c0e21e23300636de884568540a85ff382c
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-06-05 17:55:38 -0400

    Remove all CF CDN URLs and add workaround to prevent Runelic update

--- update.sh
+++ update.sh
@@ -7,6 +7,22 @@ else
 fi
 git add :
 packwiz refresh
+
+# The Runelic file must stay the same no matter what. If it got accidentally updated, revert it back.
+echo "name = \"Runelic\"
+filename = \"Runelic-Fabric-1.18.2-11.0.1.jar\"
+side = \"both\"
+
+[download]
+hash-format = \"sha1\"
+hash = \"d0c3d625bd450f4e58f8c7a5bf44d05da15690b4\"
+mode = \"metadata:curseforge\"
+
+[update]
+[update.curseforge]
+file-id = 3683667
+project-id = 393149" > mods/runelic.pw.toml
+
 git commit -a
 if [ "$1" == "push" ] || [ "$2" == "push" ]; then
   git push
